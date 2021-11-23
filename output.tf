19adf76b9db00bf05c9dc605fd3e6987c57f94b9
Riju-vv <riju.vv@progress.com>
2021-11-23 07:49:58 +0000

    adding security group

--- output.tf
+++ output.tf
@@ -21,3 +21,7 @@ value = aws_route_table_association.my-route-association.id
 output "my_keyname" {
 value = aws_key_pair.vm_ssh_key.key_name
 }
+
+output "security_group_id_array" {
+value = aws_security_group.open_ports.*.id
+}
