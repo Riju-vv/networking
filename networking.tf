625086ce8f61c0aa4467113ebad86746fcedbb9f
Riju-vv <riju.vv@progress.com>
2021-11-23 05:46:39 +0000

    adding subnet

--- networking.tf
+++ networking.tf
@@ -5,3 +5,12 @@ tags = {
 Name = "student.09-vpc"
 }
 }
+
+resource "aws_subnet" "subnet-1" {
+vpc_id = aws_vpc.main-vpc.id
+cidr_block = "10.100.0.0/24"
+map_public_ip_on_launch = "true"
+tags = {
+Name = "student.09-subnet-1"
+}
+}
